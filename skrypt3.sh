12104b0c0c2bf5e38da483317aafe09fef3b65d4
Mateusz Adamowski <m.adamowski@alx.pl>
2021-03-30 13:11:32 +0000

    skrypt 3

---
+++ skrypt3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#echo "Dzień dobry"
+echo "Dziś jest $(date +%F)"
+echo HEJ
+echo HEJ
+echo HELO
+echo "Liczba użytkowników z bashem: $(cat /etc/passwd|grep bash|wc -l)"
+#echo "Zalogowani na serwerze:"
+#finger
+
+
