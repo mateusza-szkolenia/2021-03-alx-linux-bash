dbc6fc2d892d5317130a3dd4e32b238f8ed134f1
Mateusz Adamowski <m.adamowski@alx.pl>
2021-03-30 13:39:35 +0000

    sterowanie pętlą

--- petla4.sh
+++ petla4.sh
@@ -2,6 +2,17 @@
 
 for raport in raport-{2015..2021}-{01..12}.txt
 do
+
+	if [ "$raport" == "raport-2021-03.txt" ]
+	then
+		break
+	fi
+
+	if [ "$raport" == "raport-2018-05.txt" ]
+	then
+		continue
+	fi
+
 	echo "Tworzę plik raportu: $raport"
 
 done
